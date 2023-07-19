069121d9c0c3160656f0c97e477f8a55b9f5080b
Mia Yamada-Heidner <mia.yamada-heidner@ericsson.com>
2023-07-19 14:00:51 -0700

    tested all functions

--- replace_node.py
+++ replace_node.py
@@ -10,27 +10,31 @@ import amoshell
 import sys
 import os
 
+CREATION_FILES = ['radioCreation.mos', 'rfPortCreation.mos', 'cellCreation.mos', 
+                  'sectorCreation.mos', 'carrierCreation.mos', 'antennaCreation.mos',
+                  'rilinkCreation.mos']
+
 def check_status(rval, err):
     if rval:
         print(err)
         sys.exit(1)
 
-def create_radio():
+def create_node():
     mo = amoshell.Amos()
     # generate a .mos file to create the new MO
-    rval, out, err = mo.moshell('WTC1EENB84', 'lt all; run getRadioInfo.mos radioInfo.txt radioCreation.mos')
+    rval, out, err = mo.moshell('WTC1EENB84', 'lt all; run collectNodeInfo.mos cmds.mos')
     check_status(rval=rval, err=err)
-    
-    # replace all occurences of given old name with given new name 
+
+    # replace all occurences of given old name with given new name
     # e.g. "RRU-1" to be replaced with "RRU-10"
     old_name = sys.argv[1]
     new_name = sys.argv[2]
-    replace_cmd = "sed -i s/{}/{}/g radioCreation.mos".format(old_name, new_name)
-    os.system(replace_cmd)
-    
-    # create new MO
-    rval, out, err = mo.moshell('WTC1EENB84', 'run radioCreation.mos; lt all')
-    check_status(rval=rval, err=err)
+    for f in CREATION_FILES:
+        replace_cmd = "sed -i s/{}/{}/g {}".format(old_name, new_name, f)
+        os.system(replace_cmd)
+        create_cmd = "run {}; lt all".format(f)
+        rval, out, err = mo.moshell('WTC1EENB84', create_cmd)
+        check_status(rval=rval, err=err)
 
 if __name__ == '__main__':
-    create_radio()
+    create_node()
