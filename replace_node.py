14ddf2983d6f4c6c2887503f03cf09d95aa0f4a5
Mia Yamada-Heidner <mia.yamada-heidner@ericsson.com>
2023-07-20 16:27:27 -0700

    fixing function to create cell

--- replace_node.py
+++ replace_node.py
@@ -10,31 +10,33 @@ import amoshell
 import sys
 import os
 
-CREATION_FILES = ['radioCreation.mos', 'rfPortCreation.mos', 'cellCreation.mos', 
-                  'sectorCreation.mos', 'carrierCreation.mos', 'antennaCreation.mos',
-                  'rilinkCreation.mos']
+CREATION_FILES = ['radioCreation.mos', 'rilinkCreation.mos',
+                  'antennaCreation.mos', 'sectorCreation.mos', 
+                  'carrierCreation.mos', 'cellCreation.mos']
 
 def check_status(rval, err):
     if rval:
         print(err)
         sys.exit(1)
 
-def create_node():
+def create_node(old_name, new_name):
     mo = amoshell.Amos()
     # generate a .mos file to create the new MO
+    print("Creating .mos files")
     rval, out, err = mo.moshell('WTC1EENB84', 'lt all; run collectNodeInfo.mos cmds.mos')
     check_status(rval=rval, err=err)
-
+    print("Modifying the .mos files...")
     # replace all occurences of given old name with given new name
     # e.g. "RRU-1" to be replaced with "RRU-10"
-    old_name = sys.argv[1]
-    new_name = sys.argv[2]
     for f in CREATION_FILES:
         replace_cmd = "sed -i s/{}/{}/g {}".format(old_name, new_name, f)
         os.system(replace_cmd)
         create_cmd = "run {}; lt all".format(f)
+        print("Creating new MOs...")
         rval, out, err = mo.moshell('WTC1EENB84', create_cmd)
         check_status(rval=rval, err=err)
 
 if __name__ == '__main__':
-    create_node()
+    old_name = sys.argv[1]
+    new_name = sys.argv[2]
+    create_node(old_name, new_name)
